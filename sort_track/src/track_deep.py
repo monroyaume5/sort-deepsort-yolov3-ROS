64b7ee1a307afdda5b830ba0c0cbed99338e147e
monroyaume5 <55629426+monroyaume5@users.noreply.github.com>
2022-03-25 20:32:51 -0700

    Update track_deep.py

--- sort_track/src/track_deep.py
+++ sort_track/src/track_deep.py
@@ -20,7 +20,33 @@ from sensor_msgs.msg import Image
 from sort_track.msg import IntList
 from _collections import deque
 import matplotlib.pyplot as plt
+import sys
 
+
+def imgmsg_to_cv2(img_msg):
+    #print('img_msg.encoding',img_msg.encoding)
+    if img_msg.encoding != "rgb8":
+        rospy.logerr("This Coral detect node has been hardcoded to the 'rgb8' encoding.  Come change the code if you're actually trying to implement a new camera")
+    #if img_msg.encoding != "bgr8":
+    #    rospy.logerr("This Coral detect node has been hardcoded to the 'bgr8' encoding.  Come change the code if you're actually trying to implement a new camera")
+    dtype = np.dtype("uint8") # Hardcode to 8 bits...
+    dtype = dtype.newbyteorder('>' if img_msg.is_bigendian else '<')
+    image_opencv = np.ndarray(shape=(img_msg.height, img_msg.width, 3), # and three channels of data. Since OpenCV works with bgr natively, we don't need to reorder the channels.
+                    dtype=dtype, buffer=img_msg.data)
+    # If the byt order is different between the message and the system.
+    if img_msg.is_bigendian == (sys.byteorder == 'little'):
+        image_opencv = image_opencv.byteswap().newbyteorder()
+    return image_opencv
+
+def cv2_to_imgmsg(cv_image):
+    img_msg = Image()
+    img_msg.height = cv_image.shape[0]
+    img_msg.width = cv_image.shape[1]
+    img_msg.encoding = "bgr8"
+    img_msg.is_bigendian = 0
+    img_msg.data = cv_image.tostring()
+    img_msg.step = len(img_msg.data) // img_msg.height # That double line is actually integer division, not a comment
+    return img_msg
 
 def get_parameters():
     """
@@ -59,7 +85,8 @@ def callback_image(data):
     #Display Image
     #bridge = CvBridge()
     #cv_rgb = bridge.imgmsg_to_cv2(data, "bgr8")
-    cv_rgb = cv2.cvtColor(data, cv2.COLOR_BGR2RGB)
+    cv_rgb = imgmsg_to_cv2(data)
+    #cv_rgb = cv2.cvtColor(data, cv2.COLOR_BGR2RGB)
     #Features and detections
     features = encoder(cv_rgb, detections)
 
